e2a5139134a3a0157effe5cab9e5368ee6eafc7e
rodolfoba <rodolfoba87@gmail.com>
2017-11-09 23:14:41 -0200

    Aplicando config para deploy nexus a partir do travis-ci

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -6,16 +6,16 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+    mvn --settings .travis-ci/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
         rm -rf ~/.gnupg
     fi
 
-    source .travis/gpg.sh
+    source .travis-ci/gpg.sh
 
-    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+    mvn clean deploy --settings .travis-ci/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
 
 
     if [ ! -z "$TRAVIS" ]; then
