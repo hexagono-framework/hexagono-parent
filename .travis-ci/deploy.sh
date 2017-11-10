c56ea8602e3cc25fcccf9039461f6c5d80a78b03
rodolfoba <rodolfoba87@gmail.com>
2017-11-09 23:42:46 -0200

    Ajustando config travis-ci

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -2,26 +2,24 @@
 
 set -e
 
+if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+    shred -v ~/.gnupg/*
+    rm -rf ~/.gnupg
+fi
+
+source .travis-ci/gpg.sh
+
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis-ci/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
-
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-
-    source .travis-ci/gpg.sh
-
     mvn clean deploy --settings .travis-ci/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
-
-
-    if [ ! -z "$TRAVIS" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
     mvn clean deploy --settings .travis-ci/settings.xml -DskipTests=true -B -U
+fi
+
+if [ ! -z "$TRAVIS" ]; then
+    shred -v ~/.gnupg/*
+    rm -rf ~/.gnupg
 fi
